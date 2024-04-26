82ec1c37f05b612339a0dd5fbb907e2c0190416b
NikSala10 <salamancanikol10@gmail.com>
2024-04-26 16:34:53 -0500

    script

---
+++ script.js
@@ -0,0 +1,25 @@
+const agregarPersonaje = (nombre, imagen) => {
+    const contenedor = document.getElementById("personajes");
+  
+    const personaje = document.createElement("div");
+    personaje.innerHTML = `
+    <div class="personaje">
+      <h5 class="personaje__nombre">${nombre}</h5>
+      <img class="personaje__imagen" src="${imagen}" />
+    </div>`;
+  
+    contenedor.appendChild(personaje);
+  };
+  
+  const render = async () => {
+    const response = await fetch('https://raw.githubusercontent.com/JonatanOrdonez/fundamentos-programacion-diseno/main/2024-01/Labs/Lab%2011/personajes.json');
+    data = await response.json()
+    data.personajes.forEach(personaje => {
+        agregarPersonaje(personaje.name, personaje.image);
+      });
+    // 1. Completar el código aqui para cargar el archivo json y crear los elementos HTML.
+    // 2. Para cargar el archivo json se debe usar la función fetch y luego convertir la respuesta a un objeto JSON usando la función json().
+    // 3. La función agregarPersonaje(nombre, imagen) recibe los datos del personaje y crea el elemento HTML con la información del personaje.
+  };
+  
+  document.addEventListener("DOMContentLoaded", render);
